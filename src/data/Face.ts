3f13d6a6b6080e6fe8cec576be41348f4b184586
DenFengLai <dengfenglai547@qq.com>
2025-12-29 18:19:18 +0800

    fix(Face): 修复随机表情自建图片类型不生效

--- src/data/Face.ts
+++ src/data/Face.ts
@@ -3,25 +3,44 @@ import { FacePath } from '@/dir'
 import fs from 'node:fs'
 
 /** 表情json数据 */
-const FaceJsonData = Data.getJSON<Record<string, string>>('json/FaceList.json', 'res')
+export const FaceJsonData = Data.getJSON<Record<string, string>>('json/FaceList.json', 'res')
 /** 表情包类型列表 */
-const FaceList: string[] = Object.keys(FaceJsonData)
-/** 表情别名 */
-const FaceAlias: Record<string, string[]> = Object.fromEntries(
-  Object.entries(FaceJsonData).map(([key, value]) => [key, value ? value.split(',') : []])
+export const FaceList: string[] = Object.keys(FaceJsonData)
+/**
+ * 表情别名
+ *
+ * @example
+ *{
+ *  "name1": [ "别名1", "别名2", "别名3" ],
+ *  "name2": [ "别名4" ],
+ *  "name3": []
+ *}
+ */
+export const FaceAlias: Record<string, string[]> = Object.fromEntries(
+  Object.entries(FaceJsonData)
+    .map(([key, value]) => [key, value ? value.split(',') : []])
 )
-/** 表情别名反向索引 */
-const FaceAliasIndex: Record<string, string> = Object.fromEntries(Object.entries(FaceAlias).flatMap(([k, a]) => a.map(v => [v, k])))
+/**
+ * 表情别名反向索引
+ *
+ * @example
+ * { "别名1": "name", "别名2": "name" }
+ */
+export const FaceAliasIndex: Record<string, string> = Object.fromEntries(
+  Object.entries(FaceAlias)
+    .flatMap(([k, a]) => a.map(v => [v, k]))
+)
 
 /** 载入用户自建表情文件夹 */
 void (async () => {
   if (!fs.existsSync(FacePath)) return
   try {
-    const dirNames = (await fs.promises.readdir(FacePath, { withFileTypes: true }))
+    (await fs.promises.readdir(FacePath, { withFileTypes: true }))
       .filter(dirent => dirent.isDirectory() && dirent.name !== '.git' && !dirent.name.startsWith('.'))
       .map(dirent => dirent.name)
-    dirNames.forEach(name => !FaceList.includes(name) && FaceList.push(name))
+      .forEach(name => {
+        if (!FaceList.includes(name)) FaceList.push(name)
+        FaceAlias[name] ??= []
+      })
   } catch { }
 })()
-
-export { FaceJsonData, FaceList, FaceAlias, FaceAliasIndex }
